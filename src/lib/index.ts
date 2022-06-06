0daedb6395431c561a171b4318298ef6580c6d0d
Syed Ali Murtaza <ali@kazmi.me>
2022-06-06 12:28:48 +0500

    Adds index file with lib, to manage top level exports in public-api.ts

---
+++ src/lib/index.ts
@@ -0,0 +1,12 @@
+/*
+ * Public API Surface of core
+ */
+export * from './common.module';
+export * from './base.sandbox';
+export * from './animations';
+export * from './components';
+export * from './directive';
+export * from './helpers';
+export * from './models';
+export * from './pipes';
+export * from './ui';
--- src/public-api.ts
+++ src/public-api.ts
@@ -1,12 +1,4 @@
 /*
  * Public API Surface of core
  */
-export * from './lib/common.module';
-export * from './lib/base.sandbox';
-export * from './lib/animations';
-export * from './lib/components';
-export * from './lib/directive';
-export * from './lib/helpers';
-export * from './lib/models';
-export * from './lib/pipes';
-export * from './lib/ui';
+export * from './lib';
